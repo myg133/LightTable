285a39b6e2b90602cc9c144bdb143e832259d806
michael <myg133@hotmail.com>
2018-04-01 00:32:20 +0800

    1. fix codemirror addon 2. update Clojure plugin

--- script/build.sh
+++ script/build.sh
@@ -46,7 +46,7 @@ lein cljsbuild once cljsdeps
 
 
 # Fetch plugins
-PLUGINS=("Clojure,0.3.1" "CSS,0.0.6" "HTML,0.1.0" "Javascript,0.2.0"
+PLUGINS=("Clojure,0.3.3" "CSS,0.0.6" "HTML,0.1.0" "Javascript,0.2.0"
          "Paredit,0.0.4" "Python,0.0.7" "Rainbow,0.0.8")
 
 # Plugins cache
